d503b61967f9de6d949580a7b02d46042fac7a2b
Nirmal <technktailor@gmail.com>
2023-10-26 15:53:26 +0530

    Added JSON File and First Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:3004/views";
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    if (data.length !== 0) {
+      setIsLoading(false);
+    }
+    console.log(data);
+  }, [data]);
+
+  const renderDataViews = (data) => {
+    console.log("received data", data);
+    let arrayItems = [];
+    data.map((views) =>
+      arrayItems.push(
+        <div key={views.id}>
+          <span>{views.name}</span>
+          <span>{views.discription}</span>
+          <span>{views.returns}</span>
+          <span>{views.volitility}</span>
+        </div>
+      )
+    );
+    console.log("arrayItems", arrayItems);
+    return arrayItems;
+  };
+
+  return <div>{isLoading ? <h2>Loading...</h2> : renderDataViews(data)}</div>;
 }
 
 export default App;
